9fce70934cf7e0928c077d2cefe6ddd5e8e35f13
Bohdan <48392586+BohaLash@users.noreply.github.com>
2025-06-22 22:09:54 +0200

    refactor: change hasOwnProperty to Object.hasOwn

--- src/worker-templates/sitemaps-worker.ts
+++ src/worker-templates/sitemaps-worker.ts
@@ -3,7 +3,7 @@ export default {
     const url = new URL(request.url);
     const responses: { [path: string]: string } = {}; // RESPONSES
     // if no sitemap file exists - pass to origin
-    if (!responses.hasOwnProperty(url.pathname)) return fetch(request);
+    if (!Object.hasOwn(responses, url.pathname)) return fetch(request);
     return new Response(responses[url.pathname], {
       headers: { "content-type": "application/xml; charset=UTF-8" },
     });
