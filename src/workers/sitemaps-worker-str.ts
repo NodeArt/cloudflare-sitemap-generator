9eebd865310a0ff312f5e9a006d5c08a3ea6e9af
Bohdan <48392586+BohaLash@users.noreply.github.com>
2025-06-30 19:53:37 +0200

    V2.2.1 (#6)
    * feat: v2.2.1

---
+++ src/workers/sitemaps-worker-str.ts
@@ -0,0 +1,16 @@
+export default {
+  async fetch (request): Promise<Response> {
+    try {
+      const router: { [path: string]: string } = { /* SITEMAPS_ROUTER */ }
+      const url = new URL(request.url)
+      if (!Object.hasOwn(router, url.pathname)) return await fetch(request)
+      const response = router[url.pathname]
+      const headers = { 'content-type': 'application/xml; charset=UTF-8' }
+      return new Response(response, { headers })
+    } catch (err) {
+      console.log(request)
+      console.error(err)
+      return await fetch(request)
+    }
+  }
+} satisfies ExportedHandler
