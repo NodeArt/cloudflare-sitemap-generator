8379c184510ca140d29d1234c59c5882a0f8ebe4
Bohdan <48392586+BohaLash@users.noreply.github.com>
2025-07-05 22:26:12 +0200

    v2.2.2 (#7)
    * feat: v2.2.2

--- src/workers/sitemaps-worker-with-replace.ts
+++ src/workers/sitemaps-worker-with-replace.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch (request): Promise<Response> {
     try {
-      const router: { [path: string]: string } = { /* SITEMAPS_ROUTER */ }
+      const router: { [path: string]: string } = { /* SITEMAPS_ROUTER */ } // this is supposed to be matched with regex `\{\s*\/\* SITEMAPS_ROUTER \*\/\s*\}` do not change the code in a way that will break this
       const url = new URL(request.url)
       if (!Object.hasOwn(router, url.pathname)) return await fetch(request)
       const response = router[url.pathname]
